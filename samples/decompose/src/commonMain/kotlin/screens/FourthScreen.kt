8c568d9c4bca80d621785347c160172744e2c665
Theodoro Mota <theolm.mota@gmail.com>
2024-04-08 20:28:06 -0300

    Fixing wrong deeplink

--- samples/decompose/src/commonMain/kotlin/screens/FourthScreen.kt
+++ samples/decompose/src/commonMain/kotlin/screens/FourthScreen.kt
@@ -6,19 +6,18 @@ import androidx.compose.runtime.Composable
 import components.DemoScaffold
 import dev.theolm.rinku.Rinku
 
-
 @Composable
-    fun FourthScreen(onBackPress: () -> Unit) {
-        DemoScaffold(
-            title = "Fourth",
-            onBackPress = onBackPress
-        ) {
-            Text("Fourth Screen")
+fun FourthScreen(onBackPress: () -> Unit) {
+    DemoScaffold(
+        title = "Fourth",
+        onBackPress = onBackPress
+    ) {
+        Text("Fourth Screen")
 
-            Button(onClick = {
-                Rinku.handleDeepLink("rinku://dev.theolm.rinku/fourth/second/third/second")
-            }) {
-                Text("New deeplink")
-            }
+        Button(onClick = {
+            Rinku.handleDeepLink("rinku://dev.theolm.rinku/fourth/third/second")
+        }) {
+            Text("New deeplink")
         }
     }
+}
