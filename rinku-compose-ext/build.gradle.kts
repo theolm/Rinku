b8b37ea97525c34804c5416178f71cf0f6651d45
Theodoro Mota <theolm.mota@gmail.com>
2024-04-08 20:11:53 -0300

    Enabling lib publish

--- rinku-compose-ext/build.gradle.kts
+++ rinku-compose-ext/build.gradle.kts
@@ -1,10 +1,11 @@
+import com.vanniktech.maven.publish.SonatypeHost
 import config.Config
 import plugins.setupKmpTargets
 
 plugins {
     id("android-lib-setup")
     id("compose-module-setup")
-//    id("publish-setup")
+    id("publish-setup")
     id("detekt-setup")
 }
 
@@ -32,19 +33,19 @@ kotlin {
     }
 }
 
-//mavenPublishing {
-//    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
-//    signAllPublications()
-//
-//    val version = System.getenv("VERSION") ?: Config.libVersion
-//    coordinates(
-//        groupId = Config.groupId,
-//        artifactId = Config.artifactId,
-//        version = version
-//    )
-//
-//    pom {
-//        name.set("Rinku")
-//        description.set("Deep Link Handling for Kotlin Multiplatform.")
-//    }
-//}
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+
+    val version = System.getenv("VERSION") ?: Config.libVersion
+    coordinates(
+        groupId = Config.groupId,
+        artifactId = Config.artifactId + "-compose-ext",
+        version = version
+    )
+
+    pom {
+        name.set("Rinku Compose Extensions")
+        description.set("Compose extensions to facilitate the usage of Rinku in Compose multiplatform projects.")
+    }
+}
