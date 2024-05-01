96d3d3d0b08d9bc749117aa01de4ee7716c017fd
Theodoro Mota <theolm.mota@gmail.com>
2024-05-01 19:57:38 -0300

    Adding documentation

--- rinku-core/src/commonMain/kotlin/dev/theolm/rinku/models/DeepLinkFilter.kt
+++ rinku-core/src/commonMain/kotlin/dev/theolm/rinku/models/DeepLinkFilter.kt
@@ -1,5 +1,12 @@
 package dev.theolm.rinku.models
 
+/**
+ * Interface to filter external deep links.
+ * Use case: filter out deep links that are not meant to be handled by the app.
+ * Instead of adding a path filter in the androidmanifest or info.plist, you can use
+ * this interface to filter out deep links.
+ * The application will open, but isValid return false, the deeplink will not be handled by Rinku.
+ */
 interface DeepLinkFilter {
     fun isValid(url: String): Boolean
 }
--- rinku-core/src/commonMain/kotlin/dev/theolm/rinku/models/DeepLinkMapper.kt
+++ rinku-core/src/commonMain/kotlin/dev/theolm/rinku/models/DeepLinkMapper.kt
@@ -1,5 +1,11 @@
 package dev.theolm.rinku.models
 
+/**
+ * Interface to map external deep links into internal deeplinks
+ * UseCase: Android and ios application has different deeplinks registered in marketing campaigns.
+ * The mapper can be used to map the external deeplink to unique internal deeplink,
+ * and the application can handle the deeplink accordingly in a unified way.
+ */
 interface DeepLinkMapper {
     fun map(url: String): String
 }
