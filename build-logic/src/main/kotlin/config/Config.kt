0d97093075755ae8ea501c9a2edae5e8d9865b58
Theodoro Mota <theolm.mota@gmail.com>
2024-09-07 21:21:32 -0300

    Updating dependencies

--- build-logic/src/main/kotlin/config/Config.kt
+++ build-logic/src/main/kotlin/config/Config.kt
@@ -14,5 +14,5 @@ object Config {
     // Libraries versions
     const val artifactId = "rinku"
     const val groupId = "dev.theolm"
-    const val libVersion = "1.0.0"
+    const val libVersion = "1.0.1"
 }
