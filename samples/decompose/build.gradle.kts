8b12d42a3a9fa78c249ed9c492b7d0ae7bcd7b6a
Theodoro Mota <theolm.mota@gmail.com>
2024-11-15 13:06:12 -0300

    Updating samples gradle

--- samples/decompose/build.gradle.kts
+++ samples/decompose/build.gradle.kts
@@ -36,6 +36,7 @@ kotlin {
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material)
+            implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(libs.decompose)
