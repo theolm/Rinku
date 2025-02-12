9e77695bde9aa2b3930ef14c01c7da27b5e7f550
Theodoro Mota <theolm.mota@gmail.com>
2025-02-12 20:15:08 -0300

    Rolling back AGP version

--- build-logic/src/main/kotlin/config/Config.kt
+++ build-logic/src/main/kotlin/config/Config.kt
@@ -14,5 +14,5 @@ object Config {
     // Libraries versions
     const val artifactId = "rinku"
     const val groupId = "dev.theolm"
-    const val libVersion = "1.3.3"
+    const val libVersion = "1.3.4"
 }
