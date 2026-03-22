fdb167b826bdb83bf9e9706fcb4bb07a7b1ff378
Theodoro Mota <theolm.mota@gmail.com>
2026-03-22 17:25:17 -0300

    Updating version to 1.6.0

--- build-logic/src/main/kotlin/config/Config.kt
+++ build-logic/src/main/kotlin/config/Config.kt
@@ -14,5 +14,5 @@ object Config {
     // Libraries versions
     const val artifactId = "rinku"
     const val groupId = "dev.theolm"
-    const val libVersion = "1.4.0"
+    const val libVersion = "1.6.0"
 }
