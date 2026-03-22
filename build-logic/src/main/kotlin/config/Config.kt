8a57adfcaffab8feb03e1c2193fe51bfe8c3122e
Theodoro Mota <theolm.mota@gmail.com>
2026-03-22 17:23:27 -0300

    Update Ktor dependency to version 3.2.3

--- build-logic/src/main/kotlin/config/Config.kt
+++ build-logic/src/main/kotlin/config/Config.kt
@@ -4,9 +4,9 @@ import org.gradle.api.JavaVersion
 
 object Config {
     const val applicationId = "dev.theolm.rinku"
-    const val minSdk = 22
+    const val minSdk = 23
     const val targetSdk = 35
-    const val compileSdk = 35
+    const val compileSdk = 36
     const val versionCode = 1
     const val versionName = "1.0.0"
     val javaVersion = JavaVersion.VERSION_17
